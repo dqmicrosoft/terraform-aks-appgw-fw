d9da8bc29321ab8bf48b3505a4402fa41c99abc5
Diogo Quaresma <dquaresma@microsoft.com>
2024-02-05 15:05:58 +0000

    remove storage account key from kubernetes data

--- kubernetes/data.tf
+++ kubernetes/data.tf
@@ -5,6 +5,6 @@ data "terraform_remote_state" "aks" {
     resource_group_name  = "terraform"
     storage_account_name = "terraformdquaresma"
     container_name       = "akstfstate"
-    key                  = "7+LiTV+u+n6yZ3djdyLcYQIfpXOM9C3UNJ3vxIr233fHwB9ff9uD+ugXHPxZRXz/Va+pYLCXfk6U+AStJRBQ+A=="
+    key                  = "YOURKEY"
   }
 }
